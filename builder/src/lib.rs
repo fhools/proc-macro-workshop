d36370f3d1f4ee748595a851b35f369d1c6bae28
fhools <fhools@gmail.com>
2021-05-15 16:09:45 -0700

    builder/01-parse.rs test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,32 @@
 use proc_macro::TokenStream;
+// #[macro_export]
+// macro_rules! Builder {
+//     () => {}
+// }
+
+use syn;
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
+    let ast = syn::parse(input).unwrap();
+    impl_builder_macro(&ast)
+}
 
-    unimplemented!()
+
+fn impl_builder_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        trait BuilderMacro {
+            fn builder_macro();
+        }
+
+        impl BuilderMacro for #name {
+            fn builder_macro() {
+
+            }
+        }
+    };
+    gen.into()
 }
